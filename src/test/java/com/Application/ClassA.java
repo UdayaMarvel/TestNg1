51561d070f7438bc7d7aa3f0afdacc785e5bcc53
UdayaMarvel <udayamarvel@gmail.com>
2025-08-08 07:31:32 +0530

    Updated JavaProgram class

--- src/test/java/com/Application/ClassA.java
+++ src/test/java/com/Application/ClassA.java
@@ -14,6 +14,7 @@ public class ClassA {
 	public static void main(String[] args) {
 		ClassA a = method();
 		ClassA a1 = method();
+		ClassA b = method();
 		System.out.println(System.identityHashCode(a1));
 		System.out.println(System.identityHashCode(a));
 	}
