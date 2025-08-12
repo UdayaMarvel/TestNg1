939070bd0b6d2bf0cb79d5d42f83c4398b1dda04
ponni1502 <buviarun1502@gmail.com>
2025-08-12 11:28:17 +0530

    Revert "java update"
    This reverts commit 156aaf2c5163b6983e298fced8a4a396d18e3856.

--- src/test/java/com/ponni/Githh.java
+++ src/test/java/com/ponni/Githh.java
@@ -1,21 +1,5 @@
 package com.ponni;
 
 public class Githh {
-	public static void main(String[] args) {
-		String text="Welcome to the wonder world package";
-		int len =text.length();
-		System.out.println("length of the sentence "+len);
-
-		for(int k=0; k<text.length(); k++) {
-			char chart =text.charAt(k);
-			System.out.println(chart);
-			if(chart=='l') {
-				System.out.println("index position of "+k);
-				
-			}
-		}
-
-
-	}
 
 }
