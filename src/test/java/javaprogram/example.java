3cc10add5297a686fa1fa98346e461795f355d2b
Rahini36 <rahiniperiyasamy1304@gmail.com>
2025-08-12 15:37:34 +0530

    message

---
+++ src/test/java/javaprogram/example.java
@@ -0,0 +1,31 @@
+package javaprogram;
+
+public class example {
+	public static void main(String[]args) {
+		String a="abc11b3jh";
+		int sum=0;
+		StringBuilder currentnumber=new StringBuilder();
+		
+		for(char s:a.toCharArray()) {
+			if(Character.isDigit(s)) {
+				currentnumber.append(s);
+			}else {
+				if(currentnumber.length()>0) {
+					sum=sum+Integer.parseInt(currentnumber.toString());
+					currentnumber.setLength(0);
+					
+				}
+			}
+			if(currentnumber.length()>0) {
+				sum=sum+Integer.parseInt(currentnumber.toString());
+			}
+		}
+			
+		
+		System.out.println(sum);
+
+	}
+	
+	}
+
+
