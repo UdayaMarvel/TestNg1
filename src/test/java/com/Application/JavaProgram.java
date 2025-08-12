9a2c695b5993d660585e071488b6381ad36c8b74
Riwadeisy <--kavithavallikumar96@gmail.com>
2025-08-12 09:28:22 +0530

    changes done

--- src/test/java/com/Application/JavaProgram.java
+++ src/test/java/com/Application/JavaProgram.java
@@ -6,7 +6,7 @@ public static void main(String[] args) {
 	System.out.println("Conflict---_santhoshini");
 		System.out.println("Branch to branch conflict");
 	
-
+		System.out.println("Kavitha conflict")
 	
 	
 }
